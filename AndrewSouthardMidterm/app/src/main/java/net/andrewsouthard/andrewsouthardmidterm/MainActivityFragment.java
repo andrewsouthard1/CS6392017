d4b65ee8e5d2e3af47c81719637d0b87fb5da0f7
Andrew <andrewsouthard111@gmail.com>
2017-06-22 16:00:16 -0400

    Added midterm project

---
+++ AndrewSouthardMidterm/app/src/main/java/net/andrewsouthard/andrewsouthardmidterm/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package net.andrewsouthard.andrewsouthardmidterm;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private EditText dollars;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
